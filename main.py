23cb93d565e13c8de7fbcdab53b2c7700e53e455
Dia Garg <darjdiagarg@gmail.com>
2026-03-08 15:03:10 +0530

    Add whitespace cleaning and empty row removal

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import os
 import pandas as pd
 
-file = input("Enter CVS file path:")
-data=pd.read_csv(file)
+file = input("Enter CSV file name: ")
 
-print("\n Data Preview:\n", data)
+if not os.path.isfile(file):
+    print("Invalid file path. Please enter a CSV file.")
+else:
+    data = pd.read_csv(file, sep=",", skipinitialspace=True)
+
+    print("\nOriginal Data:")
+    print(data)
+
+    # Trim spaces from all text columns
+    data = data.apply(lambda col: col.str.strip() if col.dtype == "object" else col)
+
+    #Remove empty rows
+    data = data.dropna(how="all")
+
+    print("\nCleaned Data:")
+    print(data)
